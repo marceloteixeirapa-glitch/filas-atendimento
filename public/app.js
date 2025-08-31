20605de6752fe946cb850424fd277dde40382510
marceloteixeirapa-glitch <marcelo.teixeira.pa@gmail.com>
2025-08-31 11:33:38 -0300

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -1,8 +1,3 @@
-// app.js (simplificado)
 const socket = io();
-socket.on('connect', () => {
-  console.log('Conectado ao servidor via WebSocket');
-});
-socket.on('state:update', (state) => {
-  console.log('Estado atualizado:', state);
-});
+socket.on('connect', () => console.log('Conectado ao servidor'));
+socket.on('atualizacao', data => console.log('Estado atualizado:', data));
